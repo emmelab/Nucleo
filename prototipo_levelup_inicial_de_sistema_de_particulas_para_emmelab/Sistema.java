30e2bb69b15e7515c44090c4bd5e523497b00d71
Ignacio Siri <ignaciosiri@gmail.com>
2014-10-17 17:31:20 -0300

    Agregar y eliminar modificadores

--- prototipo_levelup_inicial_de_sistema_de_particulas_para_emmelab/Sistema.java
+++ prototipo_levelup_inicial_de_sistema_de_particulas_para_emmelab/Sistema.java
@@ -4,17 +4,17 @@ import java.util.HashMap;
 class Sistema {
   ArrayList<Modificador> modificadores;
   ArrayList<Boolean> estadoModificadores;
-  
+
   final PApplet p5;
   int tamano = 100;
   HashMap<String,Atributo[]> atributos;
-  
+
   Sistema(PApplet p5, int cantidad) {
     this.p5 = p5;
     tamano = cantidad;
     atributos = new HashMap();
   }
-  
+
   Atributo[] incluir(Atributo semilla) {
     if (atributos.containsKey( semilla.getKey() )) {
       System.out.println("Esta llave ya está en uso"+semilla.getKey());
@@ -27,10 +27,21 @@ class Sistema {
       return nuevas;
     }
   }
-  
+
   Atributo[] getAtributos(String llave) {
     return atributos.get(llave);
   }
-  
-  
+
+  void agregarModificador(Modificador modificador){
+    modificadores.add(modificador);
+  }
+ void agregarModificador(Modificador modificador, int posicion){
+   modificadores.add(posicion, modificador);
+ }
+
+ void eliminarModificador(int posicion){
+  modificadores.remove(posicion);
+}
+
+
 }
