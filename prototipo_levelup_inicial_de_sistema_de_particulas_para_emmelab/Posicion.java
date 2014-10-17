60766407e1e851933f6d2e0c2a7282cf1f06a2b4
pacoelayudante <pacoelayudante@gmail.com>
2014-10-17 15:45:25 -0300

    Creacion
    Iniciacion de Nucleo

---
+++ prototipo_levelup_inicial_de_sistema_de_particulas_para_emmelab/Posicion.java
@@ -0,0 +1,35 @@
+class Posicion extends Atributo {
+  static String key = "Posicion";
+  String getKey() {
+    return key;
+  }
+  Atributo soloPonerNewX() {
+    return new Posicion();
+  }
+  Atributo[] soloPonerNewArray(int tam) {
+    return new Posicion[tam];
+  }
+  //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  float x,y;
+  
+  Atributo[] iniciar(Sistema s, Atributo[] nuevas) {
+    int grilla = s.p5.ceil(s.p5.sqrt(nuevas.length));
+    int grillaX = grilla;
+    int grillaY = grilla;
+    for (int i=0; i<nuevas.length; i++) {
+      Posicion p = (Posicion)nuevas[i];
+      p.x = (i%grillaX) * (s.p5.width / grillaX);
+      p.y = (i/grillaY) * (s.p5.height / grillaY);
+    }
+    return nuevas;
+  }
+  /*Atributo[] iniciar(Sistema s, Atributo[] nuevas) {
+    for (Posicion p : (Posicion[])nuevas) {
+      p.x = s.p5.random(s.p5.width);
+      p.y = s.p5.random(s.p5.height);
+    }
+    return nuevas;
+  }*/
+  
+}
---
+++ prototipo_levelup_inicial_de_sistema_de_particulas_para_emmelab/Velocidad.java
@@ -0,0 +1,24 @@
+class Velocidad extends Atributo {
+  static String key = "Velocidad";
+  String getKey() {
+    return key;
+  }
+  Atributo soloPonerNewX() {
+    return new Velocidad();
+  }
+  Atributo[] soloPonerNewArray(int tam) {
+    return new Velocidad[tam];
+  }
+  //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  float direccion,magnitud;
+  
+  Atributo[] iniciar(Sistema s, Atributo[] nuevas) {
+    for (Velocidad p : (Velocidad[])nuevas) {
+      p.direccion = s.p5.random(s.p5.TWO_PI);
+      p.magnitud = s.p5.random(.05f,1);
+    }
+    return nuevas;
+  }
+  
+}
